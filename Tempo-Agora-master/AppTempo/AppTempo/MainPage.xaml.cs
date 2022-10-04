90a5b1e3b17d833833e46a49d5d1f3ef32d2ab43
giigvf <100951449+giigvf@users.noreply.github.com>
2022-10-04 11:40:01 -0300

    Add files via upload

---
+++ Tempo-Agora-master/AppTempo/AppTempo/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using AppTempo.Model;
+using AppTempo.Services;
+
+namespace AppTempo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Title = "Previsão do Tempo";
+            this.BindingContext = new Tempo();
+        }
+
+        private async void btnPrevisao_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(cidadeEntry.Text))
+                    {
+                    Tempo previsaoDoTempo = await DataServices.GetPrevisaoDoTempo(cidadeEntry.Text);
+                this.BindingContext = previsaoDoTempo;
+                    btnPrevisao.Text = "Nova Previsão";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+    }
+}
